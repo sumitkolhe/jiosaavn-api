f229aab3aed7db43d6accbd034ce0ba0ba84b69d
sumit <thesumitkolhe@gmail.com>
2020-07-18 19:58:54 +0530

    :pencil2: Added song search by name

--- index.js
+++ index.js
@@ -1,43 +1,71 @@
-const axios = require("axios");
+axios = require("axios");
 const express = require("express");
-const { response } = require("express");
+const UA = require("user-agents");
+const userAgentCreator = new UA({ deviceCategory: "desktop" });
+const url = require("./utils/url");
 const app = express();
 
-app.get("/:query", async (req, res) => {
+app.get("/:query/:count?", async (req, res) => {
+  var rotatingUserAgent = userAgentCreator.random().toString();
+  axios.defaults.headers.common["User-Agent"] = rotatingUserAgent;
 
-   var id = await getSongId(req.params.query)
-   var encryptedId = await getEncryptedSongId(id)
-    var url = await generateAuthToken(encryptedId)
-
-    res.redirect(url)
+  var query = req.params.query;
+  var count = req.params.count;
+  if (!count) count = 5;
+  console.log(count);
+  var songsArray = new Array();
+  var songsObj = new Object();
+  axios
+    .get(url.GetSongsFromSearch + query + "&n=" + count)
+    .then(async (response) => {
+      var songs = response.data.results;
+      for (element of songs) {
+        songsArray.push({
+          song_name: element.title,
+          song_id: element.id,
+          song_url: element.perma_url,
+          song_image: await fixImageUrl(element.image),
+          album_id: element.more_info.album_id,
+          album_name: element.more_info.album,
+          album_url: element.more_info.album_url,
+          artist_name: element.more_info.artistMap.artists[0].name,
+          artist_image: await fixImageUrl(
+            element.more_info.artistMap.artists[0].image
+          ),
+          year: element.year,
+          duration: element.more_info.duration,
+          language: element.language,
+          label: element.more_info.label,
+          encrypted_media_url: element.more_info.encrypted_media_url,
+          download_link: await GetDownloadLink(
+            element.more_info.encrypted_media_url
+          ),
+        });
+      }
+      songsObj["songs"] = songsArray;
+      res.json(songsObj);
+    });
 });
 
-function getSongId(query) {
-
-  var baseGetSongIdUrl = `https://www.jiosaavn.com/api.php?_format=json&_marker=0&ctx=web6dot0&__call=autocomplete.get&query=${query}`;
-
-  return axios.get(baseGetSongIdUrl).then((response) => {
-    return response.data.songs.data[0].id;
-  });
+function GetDownloadLink(encrypted_id) {
+  return axios
+    .get(url.generateAuthtoken + encodeURIComponent(encrypted_id))
+    .then((response) => {
+      return CleanDownloadLink(response.data.auth_url);
+    });
 }
 
-function getEncryptedSongId(songId) {
-
-  var baseGenerateAuthtokenUrl = `https://www.jiosaavn.com/api.php?__call=song.getDetails&pids=${songId}&api_version=4&_format=json&_marker=0&ctx=web6dot0`;
-
-  return axios.get(baseGenerateAuthtokenUrl).then((response) => {
-    return response.data.songs[0].more_info.encrypted_media_url;
-  });
+function CleanDownloadLink(auth_url) {
+  var url = auth_url.split("?")[0];
+  url = url.split("/")[3] + "/" + url.split("/")[4];
+  url = "https://h.saavncdn.com/" + url;
+  return url;
 }
 
-function generateAuthToken(encryptedMediaUrl) {
-  var encryptedId = encryptedMediaUrl;
-  encryptedId = encryptedId.replace(/[+]/g, "%2B");
-  var baseGenerateAuthtokenUrl = `https://www.jiosaavn.com/api.php?__call=song.generateAuthToken&url=${encryptedId}&bitrate=320&api_version=4&_format=json&ctx=web6dot0&_marker=0`;
-
-  return axios.get(baseGenerateAuthtokenUrl).then((response) => {
-    return response.data.auth_url;
-  });
+function fixImageUrl(img_url) {
+  var fixedImageUrl = img_url.replace("150x150", "500x500");
+  fixedImageUrl = fixedImageUrl.replace("http://", "https://");
+  return fixedImageUrl;
 }
 
 app.listen(80);
