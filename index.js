207322d7029217070c824c5bcb5a92d67bf92f93
sumit <thesumitkolhe@gmail.com>
2020-07-19 19:08:55 +0530

    :zap: Added preview, stream, mp3 and mp4 download links

--- index.js
+++ index.js
@@ -14,18 +14,22 @@ app.get("/:query/", async (req, res) => {
   var songsArray = new Array();
   var songsObj = new Object();
   axios
-    .get("https://www.jiosaavn.com/api.php?_format=json&n=6&p=1&_marker=0&ctx=android&__call=search.getResults&q=" + query)
+    .get(
+      "https://www.jiosaavn.com/api.php?_format=json&n=6&p=1&_marker=0&ctx=android&__call=search.getResults&q=" +
+        query
+    )
     .then(async (response) => {
       var songs = response.data.results;
 
       for (element of songs) {
         songsArray.push({
-          song_name: element.song,
           song_id: element.id,
+          song_title: element.song,
           song_url: element.perma_url,
           song_image: await fixImageUrl(element.image),
+          song_play_count: element.play_count,
           album_id: element.albumid,
-          album_name: element.album,
+          album_title: element.album,
           album_url: element.album_url,
           artist_name: element.primary_artists,
           year: element.year,
@@ -33,11 +37,11 @@ app.get("/:query/", async (req, res) => {
           language: element.language,
           label: element.label,
           encrypted_media_url: element.encrypted_media_url,
-          stream_link: await GetStreamLink(
-            element.encrypted_media_url
-          ),
-          download_link: await GetDownloadLink(
-            element.encrypted_media_url
+          preview_url: element.media_preview_url || null,
+          stream_link: await GetStreamLink(element.encrypted_media_url),
+          download_link_mp4: await GetDownloadLink(element.encrypted_media_url),
+          download_link_mp3: await GetDownloadLinkAlt(
+            element.media_preview_url
           ),
         });
       }
@@ -45,7 +49,6 @@ app.get("/:query/", async (req, res) => {
       res.json(songsObj);
     });
 });
-
 
 function GetStreamLink(encrypted_id) {
   return axios
@@ -59,7 +62,6 @@ function GetStreamLink(encrypted_id) {
 }
 
 function GetDownloadLink(encrypted_id) {
-  
   return axios
     .get(
       "https://www.jiosaavn.com/api.php?bitrate=320&api_version=4&_format=json&ctx=web6dot0&_marker=0&__call=song.generateAuthToken&url=" +
@@ -69,17 +71,26 @@ function GetDownloadLink(encrypted_id) {
       return CleanDownloadLink(response.data.auth_url);
     });
 }
+
+function GetDownloadLinkAlt(media_preview_url) {
+  if (media_preview_url) {
+    var url = media_preview_url.replace("preview", "h");
+    url = url.replace("_96_p.mp4", "_320.mp3");
+    return url;
+  } else {
+    return null;
+  }
+}
+
 function CleanDownloadLink(auth_url) {
-  if(auth_url){
-     var url = auth_url.split("?")[0];
-  url = url.split("/")[3] + "/" + url.split("/")[4];
-  url = "https://aac.saavncdn.com/" + url;
-  return url;
+  if (auth_url) {
+    var url = auth_url.split("?")[0];
+    url = url.split("/")[3] + "/" + url.split("/")[4];
+    url = "https://aac.saavncdn.com/" + url;
+    return url;
+  } else {
+    return null;
   }
-  else{
-    return null 
-  }
- 
 }
 
 function fixImageUrl(img_url) {
