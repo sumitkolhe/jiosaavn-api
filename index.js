c1c567845825d49c348947835b34909a92c951d6
sumit <thesumitkolhe@gmail.com>
2020-07-20 10:25:24 +0530

    :construction: Major restructuring

--- index.js
+++ index.js
@@ -4,13 +4,15 @@ const UA = require("user-agents");
 const userAgentCreator = new UA({ deviceCategory: "desktop" });
 const HttpsProxyAgent = require("https-proxy-agent");
 const app = express();
+const cors = require("cors");
 //const httpsAgent = new HttpsProxyAgent({ host: "103.224.39.2", port: "82" });
+app.use(cors());
 
-app.get("/:query/", async (req, res) => {
-  var rotatingUserAgent = userAgentCreator.random().toString();
-  axios.defaults.headers.common["User-Agent"] = rotatingUserAgent;
+app.get("/songs/:query", async (req, res) => {
+  //axios.defaults.headers.common["User-Agent"] = userAgentCreator.random().toString();
 
   var query = req.params.query;
+  console.log(query);
   var songsArray = new Array();
   var songsObj = new Object();
   axios
@@ -21,34 +23,77 @@ app.get("/:query/", async (req, res) => {
     .then(async (response) => {
       var songs = response.data.results;
 
-      for (element of songs) {
-        songsArray.push({
-          song_id: element.id,
-          song_title: element.song,
-          song_url: element.perma_url,
-          song_image: await fixImageUrl(element.image),
-          song_play_count: element.play_count,
-          album_id: element.albumid,
-          album_title: element.album,
-          album_url: element.album_url,
-          artist_name: element.primary_artists,
-          year: element.year,
-          duration: element.duration,
-          language: element.language,
-          label: element.label,
-          encrypted_media_url: element.encrypted_media_url,
-          preview_url: element.media_preview_url || null,
-          stream_link: await GetStreamLink(element.encrypted_media_url),
-          download_link_mp4: await GetDownloadLink(element.encrypted_media_url),
-          download_link_mp3: await GetDownloadLinkAlt(
-            element.media_preview_url
-          ),
-        });
+      console.log(response.config);
+      if (songs) {
+        for (element of songs) {
+          songsArray.push({
+            song_id: element.id,
+            song_title: element.song,
+            song_url: element.perma_url,
+            song_image: await fixImageUrl(element.image),
+            song_play_count: element.play_count,
+            album_id: element.albumid,
+            album_title: element.album,
+            album_url: element.album_url,
+            artist_name: element.primary_artists,
+            year: element.year,
+            duration: element.duration,
+            language: element.language,
+            label: element.label,
+            encrypted_media_url: element.encrypted_media_url,
+            preview_url: element.media_preview_url,
+            stream_link: await GetStreamLink(element.encrypted_media_url),
+            download_link: element.media_preview_url
+              ? await GetDownloadLinkFromPreview(element.media_preview_url)
+              : await GetDownloadLinkFromAuthToken(element.encrypted_media_url),
+          });
+        }
+        songsObj["songs"] = songsArray;
+        res.json(songsObj);
+      } else {
+        res.json({ error: "please try again after sometime" });
       }
-      songsObj["songs"] = songsArray;
-      res.json(songsObj);
     });
 });
+
+function GetDownloadLinkFromAuthToken(encrypted_id) {
+  return axios
+    .get(
+      "https://www.jiosaavn.com/api.php?bitrate=320&api_version=4&_format=json&ctx=web6dot0&_marker=0&__call=song.generateAuthToken&url=" +
+        encodeURIComponent(encrypted_id)
+    )
+    .then((response) => {
+      return CleanDownloadLink(response.data.auth_url);
+    })
+    .catch((err) => {
+      return "error";
+    });
+}
+
+function GetDownloadLinkFromPreview(media_preview_url) {
+  var url = media_preview_url.replace("preview", "aac");
+  url = url.replace("_96_p.mp4", "_320.mp3");
+
+  return axios
+    .head(url)
+    .then((response) => {
+      return url;
+    })
+    .catch((err) => {
+      var url = media_preview_url.replace("preview", "aac");
+      url = url.replace("_96_p.mp4", "_320.mp4");
+      return url;
+    });
+}
+
+function CleanDownloadLink(auth_url) {
+  if (auth_url) {
+    var url = auth_url.split("?")[0];
+    url = url.split("/")[3] + "/" + url.split("/")[4];
+    url = "https://aac.saavncdn.com/" + url;
+    return url;
+  }
+}
 
 function GetStreamLink(encrypted_id) {
   return axios
@@ -58,39 +103,10 @@ function GetStreamLink(encrypted_id) {
     )
     .then((response) => {
       return response.data.auth_url;
+    })
+    .catch((err) => {
+      return "error";
     });
-}
-
-function GetDownloadLink(encrypted_id) {
-  return axios
-    .get(
-      "https://www.jiosaavn.com/api.php?bitrate=320&api_version=4&_format=json&ctx=web6dot0&_marker=0&__call=song.generateAuthToken&url=" +
-        encodeURIComponent(encrypted_id)
-    )
-    .then((response) => {
-      return CleanDownloadLink(response.data.auth_url);
-    });
-}
-
-function GetDownloadLinkAlt(media_preview_url) {
-  if (media_preview_url) {
-    var url = media_preview_url.replace("preview", "h");
-    url = url.replace("_96_p.mp4", "_320.mp3");
-    return url;
-  } else {
-    return null;
-  }
-}
-
-function CleanDownloadLink(auth_url) {
-  if (auth_url) {
-    var url = auth_url.split("?")[0];
-    url = url.split("/")[3] + "/" + url.split("/")[4];
-    url = "https://aac.saavncdn.com/" + url;
-    return url;
-  } else {
-    return null;
-  }
 }
 
 function fixImageUrl(img_url) {
