a3bab3741e2461bb192ab34c8a564946d2a21671
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-02-20 20:11:48 +0530

    :pencil2: Added docs link

--- api/index.ts
+++ api/index.ts
@@ -2,8 +2,9 @@ import { NowRequest, NowResponse } from "@vercel/node";
 
 module.exports = async (_req: NowRequest, res: NowResponse) => {
   res.json({
-    server: "online",
-    Documentation: "https://github.com/sumitkolhe/jiosaavn-api",
+    Server: "online",
+    Documentation: "https://docs.saavn.me",
+    Github_repo: "https://github.com/sumitkolhe/jiosaavn-api",
+    Author: "https://sumit.codes",
   });
 };
- 
