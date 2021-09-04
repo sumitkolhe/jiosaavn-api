5e3cce94ef280cee516fdc6c7e09f33a270b54d9
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2021-09-04 13:37:05 +0530

    :up: remove deprecated code

--- api/home.ts
+++ api/home.ts
@@ -1,16 +1,17 @@
-import { NowRequest, NowResponse } from "@vercel/node";
-import { generateAlbumPayload, generateSongPayload } from "../utils/payload";
-import { AxiosResponse } from "axios";
+import { VercelRequest, VercelResponse } from "@vercel/node";
 import { axiosInstance } from "../utils/config";
 import { getHomeUrl } from "../utils/endpoints";
 import { setHeaders } from "../utils/headers";
 
-module.exports = async (req: NowRequest, res: NowResponse) => {
+module.exports = async (_req: VercelRequest, res: VercelResponse) => {
   setHeaders(res);
   try {
-    await axiosInstance.get(getHomeUrl()).then(resp => resp.data).then(data => {
-      res.json(data);
-    });
+    await axiosInstance
+      .get(getHomeUrl())
+      .then((resp) => resp.data)
+      .then((data) => {
+        res.json(data);
+      });
   } catch (error) {
     res.status(500).json({
       message: "something went wrong",
