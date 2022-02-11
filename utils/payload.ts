343dc4cebf5e0d46fa4eb7c20ef4e4a4f805ac51
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-11 22:36:03 +0530

    fix: variable casing

--- utils/payload.ts
+++ utils/payload.ts
@@ -1,57 +1,57 @@
 import { albumDetails, songDetails } from 'types'
 
-export const getDownloadLinks = (song_download_link: string) => {
-  if (song_download_link)
+export const getDownloadLinks = (songDownloadLink: string) => {
+  if (songDownloadLink)
     return [
-      song_download_link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_96'),
-      song_download_link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_160'),
-      song_download_link.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_320'),
+      songDownloadLink.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_96'),
+      songDownloadLink.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_160'),
+      songDownloadLink.replace('preview.saavncdn.com', 'aac.saavncdn.com').replace('_96_p', '_320'),
     ]
   return false
 }
 
 export const generateSongPayload = (data: any) => {
-  const song_payload: songDetails = {
-    song_id: data.id,
-    song_name: data.song,
-    album_id: data.albumid,
-    album_name: data.album,
+  const songPayload: songDetails = {
+    songId: data.id,
+    songName: data.song,
+    albumId: data.albumid,
+    albumName: data.album,
     year: data.year,
-    song_release_date: data.release_date,
-    song_duration: data.duration,
-    song_play_count: data.play_count,
-    song_language: data.language,
-    song_has_lyrics: data.has_lyrics,
-    song_artist: data.primary_artists,
-    song_image: data.image.replace('150x150', '500x500'),
-    song_link: data.perma_url,
-    album_link: data.album_url,
-    song_label: data.label,
+    songReleaseDate: data.release_date,
+    songDuration: data.duration,
+    songPlayCount: data.play_count,
+    songLanguage: data.language,
+    songHasLyrics: data.has_lyrics,
+    songArtist: data.primary_artists,
+    songImage: data.image.replace('150x150', '500x500'),
+    songLink: data.perma_url,
+    albumLink: data.album_url,
+    songLabel: data.label,
     copyright: data.copyright_text,
-    download_links: getDownloadLinks(data.media_preview_url),
+    downloadLinks: getDownloadLinks(data.media_preview_url),
   }
 
-  return song_payload
+  return songPayload
 }
 
 export const generateAlbumPayload = (data: any) => {
   const songsArray: songDetails[] = []
 
-  const album_payload: albumDetails = {
-    album_id: data.albumid,
-    album_name: data.title,
-    album_image: data.image.replace('150x150', '500x500'),
-    album_link: data.perma_url,
+  const albumPayload: albumDetails = {
+    albumId: data.albumid,
+    albumName: data.title,
+    albumImage: data.image.replace('150x150', '500x500'),
+    albumLink: data.perma_url,
     year: data.year,
-    album_artist: data.primary_artists,
-    album_language: data.language,
+    albumArtist: data.primary_artists,
+    albumLanguage: data.language,
   }
 
   if (data.songs) {
     data.songs.forEach((song: songDetails) => {
       songsArray.push(generateSongPayload(song))
     })
-    album_payload.songs = songsArray
+    albumPayload.songs = songsArray
   }
-  return album_payload
+  return albumPayload
 }
