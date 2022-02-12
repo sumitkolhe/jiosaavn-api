b134e424c000e2011eab5d0c517283119ac76892
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 14:15:37 +0530

    refactor: move validator to middleware

--- middleware/validator.ts
+++ middleware/validator.ts
@@ -1,6 +1,31 @@
-export const extractIdFromLink = (link: string, type: string): string => {
-  if (link.includes(`jiosaavn.com/${type}/`)) {
-    return link.split(`${type}/`)[1].split('/')[1].slice(0, 11)
-  }
-  return ''
+import Joi from 'joi'
+import { celebrate, Segments } from 'celebrate'
+
+const recordValidator = Joi.string().trim().length(24).alphanum().required().messages({
+  'string.empty': 'Record ID name cannot be empty',
+  'string.length': 'Record ID should be 24 characters long',
+  'string.alphanum': 'Record ID should alpha-numeric',
+})
+
+const payloadValidator = Joi.alternatives(
+  Joi.object()
+    .min(1)
+    .message('Empty object is not allowed.')
+    .custom(payloadSizeCalculator, 'calculate size')
+    .message('Payload size exceeded'),
+  Joi.array()
+    .min(1)
+    .message('Empty array is not allowed.')
+    .custom(payloadSizeCalculator, 'calculate size')
+    .message('Payload size exceeded')
+)
+
+export const validators = {
+  deleteRecordData: celebrate({
+    [Segments.HEADERS]: apiKeyValidator,
+    [Segments.PARAMS]: Joi.object().keys({
+      krateId: krateValidator,
+      recordId: recordValidator,
+    }),
+  }),
 }
