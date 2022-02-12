549b4c921523bd23a4541d9e35d38cc9750e28a5
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 17:06:43 +0530

    feat: search all endpoint

--- controller/saavn.controller.ts
+++ controller/saavn.controller.ts
@@ -1,20 +1,31 @@
 import { RequestHandler } from 'express'
-import { topChartsService } from '../services/topcharts.service'
-import { homeDataService } from '../services/homeData.service'
+import { TopChartsService } from '../services/topcharts.service'
+import { HomeDataService } from '../services/homeData.service'
+import { SearchService } from '../services/search.service'
 
 export class Controller {
   public static homeData: RequestHandler = async (_req, res, next) => {
     try {
-      const homeData = await homeDataService()
+      const homeData = await HomeDataService()
       res.json(homeData)
     } catch (error) {
       next(error)
     }
   }
 
-  public static topCharts: RequestHandler = async (req, res, next) => {
+  public static searchAll: RequestHandler = async (req, res, next) => {
     try {
-      const topCharts = await topChartsService()
+      const { query } = req.query
+      const topCharts = await SearchService(query as string)
+      res.json(topCharts)
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public static topCharts: RequestHandler = async (_req, res, next) => {
+    try {
+      const topCharts = await TopChartsService()
       res.json(topCharts)
     } catch (error) {
       next(error)
