32fd38c2bf9b1dbcce66717fe7ee9e1190f05bbf
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 22:24:37 +0530

    fix: song interface

--- interfaces/song.ts
+++ interfaces/song.ts
@@ -1,21 +1,24 @@
 export interface Song {
-  songId: string
-  songName: string
-  albumName: string
-  year: string | number
-  songArtist: string
-  songImage: string
-  songLabel: string
-  songLanguage: string
-  albumId: string | number
-  songReleaseDate: string | Date
-  songLink: string
-  albumLink: string
-  songPlayCount: string | number
-  copyright: string
-  songHasLyrics: string
-  songDuration: string | number
-  downloadLinks: Array<string> | boolean
+  id: string
+  song: string
+  album: string
+  year: string
+  primary_artists: string
+  image: string
+  label: string
+  albumid: string
+  language: string
+  play_count: string
+  copyright_text: string
+  explicit_content: number
+  has_lyrics: string
+  release_date: string
+  lyrics_snippet: string
+  media_preview_url: string
+  perma_url: string
+  album_url: string
+  duration: string
+  download_links: Array<string> | boolean
 }
 
 export interface SongSearch {
