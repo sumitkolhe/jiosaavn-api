1e008adaf0c45b438cce83227e9618d12a189374
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 18:53:41 +0530

    feat: add search song

--- services/search.service.ts
+++ services/search.service.ts
@@ -1,8 +1,22 @@
 import { axiosInstance } from '../config/axios'
 import { ApiType, getEndpoint } from '../config/endpoints'
 
-export const SearchService = async (query: string) => {
-  const endpoint = getEndpoint(false, ApiType.searchAll)
-  const response = await axiosInstance.get(endpoint, { params: { query } })
-  return response.data
+export class SearchService {
+  public static searchAll = async (query: string) => {
+    const endpoint = getEndpoint(false, ApiType.searchAll)
+
+    const response = await axiosInstance.get(endpoint, { params: { query } })
+
+    return response.data
+  }
+
+  public static searchSongs = async (query: string, page: string, limit: string) => {
+    const endpoint = getEndpoint(false, ApiType.searchSong)
+
+    const response = await axiosInstance.get(endpoint, {
+      params: { q: query, p: page || 1, n: limit || 20 },
+    })
+
+    return response.data
+  }
 }
