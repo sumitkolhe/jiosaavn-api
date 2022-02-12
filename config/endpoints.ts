3a2db9a5153541049741fa303282f45518ff4ec3
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 19:22:24 +0530

    feat: add getEndpoint

--- config/endpoints.ts
+++ config/endpoints.ts
@@ -1,36 +1,12 @@
-// Search by query
-export const getSongSearchUrl = (songName: string): string =>
-  `https://www.jiosaavn.com/api.php?__call=autocomplete.get&_format=json&_marker=0&includeMetaTags=1&query=${songName}`
+const BaseUrl = 'https://www.jiosaavn.com/api.php?_format=json&_marker=0&ctx=web6dot0'
 
-// home page
-export const getHomeUrl = (): string =>
-  `https://www.jiosaavn.com/api.php?__call=content.getHomepageData&_format=json&_marker=0&includeMetaTags=1`
-
-// Details
-export const getSongDetailsUrl = (songId: string): string =>
-  `https://www.jiosaavn.com/api.php?__call=song.getDetails&cc=in&_marker=0%3F_marker%3D0&_format=json&pids=${songId}`
-
-export const getAlbumDetailsUrl = (albumId: string): string =>
-  `https://www.jiosaavn.com/api.php?__call=content.getAlbumDetails&_format=json&cc=in&_marker=0%3F_marker=0&albumid=${albumId}`
-
-// Details by Perma-Token
-export const getSongDetailsByTokenUrl = (
-  token: string
-): string => `https://www.jiosaavn.com/api.php?__call=webapi.get&token=${token}&type=song&includeMetaTags=0&ctx=web6dot0&_format=json&_marker=0
-  `
-
-export const getAlbumDetailsByTokenUrl = (
-  token: string
-): string => `https://www.jiosaavn.com/api.php?__call=webapi.get&token=${token}&type=album&includeMetaTags=0&ctx=web6dot0&_format=json&_marker=0
-  `
-
-// Lyrics
-export const getLyricsUrl = (songId: string): string =>
-  `https://www.jiosaavn.com/api.php?__call=lyrics.getLyrics&ctx=web6dot0&_format=json&_marker=0%3F_marker=0&lyrics_id=${songId}`
-
-const BaseUrl = 'https://www.jiosaavn.com/api.php'
-
-export const endpoints = {
-  homeData: `${BaseUrl}?api_version=4&_format=json&_marker=0&ctx=web6dot0&__call=webapi.getLaunchData`,
-  topCharts: `${BaseUrl}?api_version=4&_format=json&_marker=0&ctx=web6dot0&__call=content.getCharts`,
+export const enum ApiType {
+  homeData = 'webapi.getLaunchData',
+  searchAll = 'autocomplete.get',
+  charts = 'content.getCharts',
+  trending = 'content.getTrending',
+  searchSong = 'search.getResults',
 }
+
+export const getEndpoint = (isVersion4: boolean, api: string) =>
+  `${BaseUrl}${isVersion4 ? '&api_version=4' : ''}&__call=${api}`
