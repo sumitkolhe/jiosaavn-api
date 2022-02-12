4d209f5c7eaf2ca0d6e5e96fe8617c367a1298f5
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 10:26:25 +0530

    feat: consume express

--- api/index.ts
+++ api/index.ts
@@ -1,11 +1,14 @@
-import { VercelRequest, VercelResponse } from '@vercel/node'
+import express from 'express'
 
-const index = async (_req: VercelRequest, res: VercelResponse) => {
+const app = express()
+
+app.get('/', (_req: express.Request, res: express.Response) => {
   res.json({
     server: 'online',
     documentation: 'https://docs.saavn.me',
     github: 'https://github.com/sumitkolhe/jiosaavn-api',
     author: 'https://sumit.co',
   })
-}
-export default index
+})
+
+export default app
