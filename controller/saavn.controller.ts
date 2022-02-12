8da345e7eb4dc7467e06570ad748ef01a2c61f04
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 21:37:25 +0530

    feat: add album search

--- controller/saavn.controller.ts
+++ controller/saavn.controller.ts
@@ -1,13 +1,14 @@
 import { RequestHandler } from 'express'
 import { TrendingService } from '../services/trending.service'
 import { ChartsService } from '../services/charts.service'
-import { HomeDataService } from '../services/homeData.service'
+import { HomeService } from '../services/home.service'
 import { SearchService } from '../services/search.service'
+import { globalConstants } from '../constants'
 
 export class Controller {
   public static homeData: RequestHandler = async (_req, res, next) => {
     try {
-      const homeData = await HomeDataService()
+      const homeData = await HomeService()
       res.json(homeData)
     } catch (error) {
       next(error)
@@ -17,8 +18,8 @@ export class Controller {
   public static searchAll: RequestHandler = async (req, res, next) => {
     try {
       const { query } = req.query
-      const topCharts = await SearchService.searchAll(query as string)
-      res.json(topCharts)
+      const searchResults = await SearchService.searchAll(query as string)
+      res.json(searchResults)
     } catch (error) {
       next(error)
     }
@@ -27,8 +28,18 @@ export class Controller {
   public static searchSongs: RequestHandler = async (req, res, next) => {
     try {
       const { query, page, limit } = req.query
-      const topCharts = await SearchService.searchSongs(query as string, page as string, limit as string)
-      res.json(topCharts)
+      const searchResults = await SearchService.searchSongs(query as string, page as string, limit as string)
+      res.json({ status: globalConstants.status.success, data: searchResults })
+    } catch (error) {
+      next(error)
+    }
+  }
+
+  public static searchAlbums: RequestHandler = async (req, res, next) => {
+    try {
+      const { query, page, limit } = req.query
+      const searchResults = await SearchService.searchAlbums(query as string, page as string, limit as string)
+      res.json(searchResults)
     } catch (error) {
       next(error)
     }
