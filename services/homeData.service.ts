d64502c2fe3e5fabab1693e087687fa4a5049ca7
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 17:06:28 +0530

    refactor: name change

--- services/homeData.service.ts
+++ services/homeData.service.ts
@@ -1,8 +1,7 @@
-/* eslint-disable no-console */
 import { axiosInstance } from '../config/axios'
 import { endpoints } from '../config/endpoints'
 
-export const homeDataService = async () => {
+export const HomeDataService = async () => {
   const response = await axiosInstance.get(endpoints.homeData)
   return response.data
 }
