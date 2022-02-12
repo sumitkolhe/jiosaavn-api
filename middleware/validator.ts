5774784e1679b753910cca034e4b01afec05872f
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-12 18:54:00 +0530

    feat: add validation for search song

--- middleware/validator.ts
+++ middleware/validator.ts
@@ -4,7 +4,14 @@ import { celebrate, Segments } from 'celebrate'
 export const validators = {
   search: celebrate({
     [Segments.QUERY]: Joi.object().keys({
-      query: Joi.string().required().min(1).message('should have a minimum length of'),
+      query: Joi.string().required().min(1),
+    }),
+  }),
+  searchSongs: celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      query: Joi.string().required().min(1),
+      page: Joi.string(),
+      limit: Joi.string(),
     }),
   }),
 }
