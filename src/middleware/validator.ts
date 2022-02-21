48b6c96ac89ee2d259ce83f32c64145f063689c9
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2022-02-21 08:53:16 +0530

    feat: add param sanitization

--- src/middleware/validator.ts
+++ src/middleware/validator.ts
@@ -4,19 +4,19 @@ import { celebrate, Segments } from 'celebrate'
 export const validators = {
   searchAll: celebrate({
     [Segments.QUERY]: Joi.object().keys({
-      query: Joi.string().required().min(1),
+      query: Joi.string().trim().required().min(1),
     }),
   }),
   searchSpecific: celebrate({
     [Segments.QUERY]: Joi.object().keys({
-      query: Joi.string().required().min(1),
-      page: Joi.string(),
-      limit: Joi.string(),
+      query: Joi.string().trim().required().min(1),
+      page: Joi.string().trim(),
+      limit: Joi.string().trim(),
     }),
   }),
   details: celebrate({
     [Segments.QUERY]: Joi.object().keys({
-      id: Joi.string().required().min(1),
+      id: Joi.string().trim().required().min(1),
     }),
   }),
 }
