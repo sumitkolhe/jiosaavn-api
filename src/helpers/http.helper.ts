5ce86a8b8ea7cccac2a32e2885a8f2286b7d1d34
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-20 18:51:06 +0530

    chore: add got for network requests

--- src/helpers/http.helper.ts
+++
@@ -1,6 +0,0 @@
-import { $fetch } from 'ofetch'
-import { getConfig } from '../configs'
-
-const { baseURL } = getConfig()
-
-export const http = $fetch.create({ baseURL })
---
+++ src/services/api.service.ts
@@ -0,0 +1,41 @@
+import got from 'got'
+import { getConfig } from '../configs'
+import type { Got } from 'got'
+import type { Config } from '../interfaces/config.interface'
+
+export class ApiService {
+  private httpClient: Got
+  private baseURL: Config['baseURL']
+  protected endpoints: Config['endpoint']
+
+  constructor() {
+    const { baseURL, endpoint } = getConfig()
+    this.baseURL = baseURL
+    this.endpoints = endpoint
+
+    this.httpClient = got.extend({
+      prefixUrl: this.baseURL,
+      searchParams: new URLSearchParams([
+        ['_format', 'json'],
+        ['_marker', '0'],
+        ['ctx', 'web6dot0'],
+      ]),
+      responseType: 'json',
+      hooks: {
+        beforeRequest: [
+          // (options) => {
+          // console.log(options.url)
+          // },
+        ],
+      },
+    })
+  }
+
+  protected http<T>(url: string, isVersion4: boolean, query?: Record<string, string | number>): Promise<T> {
+    const v4 = isVersion4 ? { api_version: 4 } : undefined
+
+    const queryParams = { ...v4, ...query }
+
+    return this.httpClient<T>({ searchParams: { __call: url, ...queryParams } }).json()
+  }
+}
