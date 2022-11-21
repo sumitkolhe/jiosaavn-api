125e173539e548cabf48b31431bccc85ef968e94
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-21 22:34:38 +0530

    fix: serverless routes

--- api/index.ts
+++ api/index.ts
@@ -1,7 +1,9 @@
 import 'dotenv/config'
 import { App } from '../src/app'
-import { IndexRoute } from '../src/routes/index.route'
+import { AlbumsRoute } from '../src/routes/albums.route'
+import { SongsRoute } from '../src/routes/songs.route'
+import { SearchRoute } from '../src/routes/search.route'
 
-const app = new App([new IndexRoute()])
+const app = new App([new SearchRoute(), new SongsRoute(), new AlbumsRoute()])
 
 export default app.getServer()
