6f8319e2539cb878db351574bda149cf625289fc
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-26 12:17:36 +0530

    fix: show remaining time after rate limit

--- src/middlewares/limiter.middleware.ts
+++ src/middlewares/limiter.middleware.ts
@@ -2,10 +2,11 @@ import { Ratelimit } from '@upstash/ratelimit' // for deno: see above
 import { Redis } from '@upstash/redis'
 import { HttpExceptionError } from '../exceptions/http.exception'
 import type { NextFunction, Request, Response } from 'express'
+import 'isomorphic-fetch'
 
 const ratelimit = new Ratelimit({
   redis: Redis.fromEnv(),
-  limiter: Ratelimit.slidingWindow(10, '30 s'),
+  limiter: Ratelimit.slidingWindow(6, '1 m'),
   // ephemeralCache: cache,
 })
 
@@ -15,10 +16,15 @@ export const rateLimiterMiddleware = async (req: Request, res: Response, next: N
 
     const userIp = xff ? (Array.isArray(xff) ? xff[0] : xff.split(',')[0]) : '127.0.0.1'
 
-    const { success } = await ratelimit.limit(`ip:${userIp}`)
+    const { success, remaining, limit, reset } = await ratelimit.limit(`ip:${userIp}`)
 
-    console.log(success)
-    if (!success) throw new HttpExceptionError(429, 'rate limit exceeded')
+    const remainingSeconds = Math.abs((Date.now() - reset) / 1000).toFixed(0)
+
+    if (!success) throw new HttpExceptionError(429, `rate limit exceeded, retry in ${remainingSeconds} seconds`)
+
+    res.set('x-ratelimit-remaining', remaining.toString())
+    res.set('x-ratelimit-limit', limit.toString())
+    res.set('x-ratelimit-reset', reset.toString())
 
     next()
   } catch (error) {
