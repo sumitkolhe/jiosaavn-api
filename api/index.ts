9763e4a6e5164b179033774c3d0f71dde2eb0132
sumitkolhe <thesumitkolhe@gmail.com>
2022-11-29 16:39:03 +0530

    feat: add homepage

--- api/index.ts
+++ api/index.ts
@@ -5,7 +5,15 @@ import { SongsRoute } from '../src/routes/songs.route'
 import { SearchRoute } from '../src/routes/search.route'
 import { PlaylistsRoute } from '../src/routes/playlists.route'
 import { ArtistsRoute } from '../src/routes/artists.route'
+import { HomeRoute } from '../src/routes/home.route'
 
-const app = new App([new SearchRoute(), new SongsRoute(), new ArtistsRoute(), new AlbumsRoute(), new PlaylistsRoute()])
+const app = new App([
+  new HomeRoute(),
+  new SearchRoute(),
+  new SongsRoute(),
+  new ArtistsRoute(),
+  new AlbumsRoute(),
+  new PlaylistsRoute(),
+])
 
 export default app.getServer()
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import 'dotenv/config'
+import { HomeRoute } from './routes/home.route'
 import { PlaylistsRoute } from './routes/playlists.route'
 import { AlbumsRoute } from './routes/albums.route'
 import { SongsRoute } from './routes/songs.route'
@@ -6,6 +7,13 @@ import { App } from './app'
 import { SearchRoute } from './routes/search.route'
 import { ArtistsRoute } from './routes/artists.route'
 
-const app = new App([new SearchRoute(), new SongsRoute(), new AlbumsRoute(), new ArtistsRoute(), new PlaylistsRoute()])
+const app = new App([
+  new HomeRoute(),
+  new SearchRoute(),
+  new SongsRoute(),
+  new AlbumsRoute(),
+  new ArtistsRoute(),
+  new PlaylistsRoute(),
+])
 
 app.listen()
