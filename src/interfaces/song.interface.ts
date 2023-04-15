c0a0f355fd75bea6945b49a7eba01f868c1ba19a
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2023-04-15 19:51:14 +0530

    fix: song types

--- src/interfaces/song.interface.ts
+++ src/interfaces/song.interface.ts
@@ -1,5 +1,22 @@
-import type { AlbumArtistResponse } from './album.interface'
 import type { DownloadLink } from './image.interface'
+
+export interface SongArtistRequest {
+  id: string
+  name: string
+  role: string
+  image: string
+  type: string
+  perma_url: string
+}
+
+export interface SongArtistResponse {
+  id: string
+  name: string
+  role: string
+  image: DownloadLink
+  type: string
+  url: string
+}
 
 export interface SongSearchRequest {
   total: number
@@ -9,51 +26,55 @@ export interface SongSearchRequest {
 
 export interface SongRequest {
   id: string
+  title: string
+  subtitle: string
+  header_desc: string
   type: string
-  song: string
-  album: string
-  year: string
-  music: string
-  music_id: string
-  primary_artists: string
-  primary_artists_id: string
-  featured_artists: string
-  featured_artists_id: string
-  singers: string
-  starring: string
-  image: string
-  label: string
-  albumid: string
-  language: string
-  origin: string
-  play_count: string
-  copyright_text: string
-  '320kbps': string
-  is_dolby_content: boolean
-  explicit_content: string
-  has_lyrics: string
-  lyrics_snippet: string
-  encrypted_media_url: string
-  encrypted_media_path: boolean
-  media_preview_url?: string
   perma_url: string
-  album_url: string
-  duration: string
-  artistMap: Record<string, string>
-  rights: {
-    code: number
-    reason: string
-    cacheable: boolean
-    delete_cached_object: boolean
+  image: string
+  language: string
+  year: string
+  play_count: string
+  explicit_content: string
+  list_count: string
+  list_type: string
+  list: string
+  more_info: {
+    music: string
+    album_id: string
+    album: string
+    label: string
+    origin: string
+    is_dolby_content: boolean
+    '320kbps': string
+    encrypted_media_url: string
+    encrypted_cache_url: string
+    album_url: string
+    duration: string
+    rights: {
+      code: string
+      cacheable: string
+      delete_cached_object: string
+      reason: string
+    }
+    cache_state: string
+    has_lyrics: string
+    lyrics_snippet: string
+    starred: string
+    copyright_text: string
+    artistMap: {
+      primary_artists: SongArtistRequest[]
+      featured_artists: SongArtistRequest[]
+      artists: SongArtistRequest[]
+    }
+    release_date: string
+    label_url: string
+    triller_available: boolean
+    is_ringtone_available: boolean
+    request_jiotune_flag: boolean
+    webp: string
+    lyrics_id: string
   }
-  webp?: boolean
-  cache_state: string
-  starred: string
-  release_date: any
-  vcode?: string
-  vlink?: string
-  triller_available: boolean
-  label_url: string
 }
 
 export interface SongSearchResponse {
@@ -65,6 +86,7 @@ export interface SongSearchResponse {
 export interface SongResponse {
   id: string
   name: string
+  subtitle: string
   type: string
   album: {
     id: string
@@ -73,16 +95,21 @@ export interface SongResponse {
   }
   year: string
   releaseDate: string
-  duration: string
+  duration: number
   label: string
-  primaryArtists: string | AlbumArtistResponse[]
-  primaryArtistsId: string
-  featuredArtists: string | AlbumArtistResponse[]
-  featuredArtistsId: string
-  explicitContent: string
-  playCount: string
+  artists: {
+    all: SongArtistResponse[]
+    primary: SongArtistResponse[]
+    featured: SongArtistResponse[]
+  }
+  explicitContent: boolean
+  playCount: number
   language: string
-  hasLyrics: string
+  lyrics: {
+    hasLyrics: boolean
+    lyricsSnippet: string
+    lyricsId: string
+  }
   url: string
   copyright: string
   image: DownloadLink
