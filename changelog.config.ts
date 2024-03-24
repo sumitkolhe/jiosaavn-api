c82a1c39f1528c84a34f6f40363defadce4c1cfb
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-03-24 10:49:57 +0530

    chore: add changelogen config

---
+++ changelog.config.ts
@@ -0,0 +1,16 @@
+export default () => ({
+  types: {
+    feat: { title: '🚀 Enhancements', semver: 'minor' },
+    perf: { title: '🔥 Performance', semver: 'patch' },
+    fix: { title: '🩹 Fixes', semver: 'patch' },
+    examples: { title: '🏀 Examples' },
+    docs: { title: '📖 Documentation', semver: 'patch' },
+    types: { title: '🌊 Types', semver: 'patch' },
+    refactor: false,
+    build: false,
+    chore: false,
+    test: false,
+    style: false,
+    ci: false
+  }
+})
