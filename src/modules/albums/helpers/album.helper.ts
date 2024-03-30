a0f43fbc11a8ae1287e6d296f85b8fe985e2aadc
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-03-30 23:58:29 +0530

    fix: make album fields nullable

--- src/modules/albums/helpers/album.helper.ts
+++ src/modules/albums/helpers/album.helper.ts
@@ -9,17 +9,17 @@ export const createAlbumPayload = (album: z.infer<typeof AlbumAPIResponseModel>)
   name: album.title,
   description: album.header_desc,
   type: album.type,
-  year: Number(album.year || 0),
-  playCount: Number(album.play_count),
+  year: album.year ? Number(album.year) : null,
+  playCount: album.play_count ? Number(album.play_count) : null,
   language: album.language,
   explicitContent: album.explicit_content === '1',
   url: album.perma_url,
-  songCount: Number(album.more_info?.song_count || 0),
+  songCount: album.more_info.song_count ? Number(album.more_info.song_count) : 0,
   artists: {
     primary: album.more_info?.artistMap?.primary_artists?.map(createArtistMapPayload),
     featured: album.more_info?.artistMap?.featured_artists?.map(createArtistMapPayload),
     all: album.more_info?.artistMap?.artists?.map(createArtistMapPayload)
   },
   image: createImageLinks(album.image),
-  ...(album.list && { songs: album.list.map((song) => createSongPayload(song)) })
+  songs: (album.list && album.list?.map(createSongPayload)) || null
 })
