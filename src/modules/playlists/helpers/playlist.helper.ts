e3e08c613c5bee8aa7401ac97c8b04e6835ce526
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-03-30 23:58:56 +0530

    feat: add separate model for search playlists

--- src/modules/playlists/helpers/playlist.helper.ts
+++ src/modules/playlists/helpers/playlist.helper.ts
@@ -1,23 +1,41 @@
 import type { z } from 'zod'
 import type { PlaylistAPIResponseModel, PlaylistModel } from '#modules/playlists/models'
+import type { SearchPlaylistAPIResponseModel, SearchPlaylistModel } from '#modules/search/models'
 import { createSongPayload } from '#modules/songs/helpers'
 import { createImageLinks } from '#common/helpers'
 import { createArtistMapPayload } from '#modules/artists/helpers'
 
 export const createPlaylistPayload = (
-  album: z.infer<typeof PlaylistAPIResponseModel>
+  playlist: z.infer<typeof PlaylistAPIResponseModel>
 ): z.infer<typeof PlaylistModel> => ({
-  id: album.id,
-  name: album.title,
-  description: album.header_desc,
-  type: album.type,
-  year: Number(album.year || 0),
-  playCount: Number(album.play_count),
-  language: album.language,
-  explicitContent: album.explicit_content === '1',
-  url: album.perma_url,
-  songCount: Number(album.list_count || 0),
-  artists: album.more_info.artists?.map(createArtistMapPayload),
-  image: createImageLinks(album.image),
-  ...(album.list && { songs: album.list.map((song) => createSongPayload(song)) })
+  id: playlist.id,
+  name: playlist.title,
+  description: playlist.header_desc,
+  type: playlist.type,
+  year: playlist.year ? Number(playlist.year) : null,
+  playCount: playlist.play_count ? Number(playlist.play_count) : null,
+  language: playlist.language,
+  explicitContent: playlist.explicit_content === '1',
+  url: playlist.perma_url,
+  songCount: playlist.list_count ? Number(playlist.list_count) : null,
+  artists: playlist.more_info.artists?.map(createArtistMapPayload) || null,
+  image: createImageLinks(playlist.image),
+  songs: (playlist.list && playlist.list?.map(createSongPayload)) || null
+})
+
+export const createSearchPlaylistPayload = (
+  playlist: z.infer<typeof SearchPlaylistAPIResponseModel>
+): z.infer<typeof SearchPlaylistModel> => ({
+  total: Number(playlist.total),
+  start: Number(playlist.start),
+  results: playlist.results.map((item) => ({
+    id: item.id,
+    name: item.title,
+    type: item.type,
+    image: createImageLinks(item.image),
+    url: item.perma_url,
+    songCount: item.more_info.song_count ? Number(item.more_info.song_count) : null,
+    language: item.more_info.language,
+    explicitContent: item.explicit_content === '1'
+  }))
 })
