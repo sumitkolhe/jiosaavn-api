9095b063f8cc36e1f29aa6fc3772986f601098fd
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2024-03-31 13:47:04 +0530

    refactor: add configurable context for all requests

--- src/common/helpers/fetch.helper.ts
+++ src/common/helpers/fetch.helper.ts
@@ -1,11 +1,23 @@
-export const useFetch = async <T>(type: string, params: Record<string, string | number>): Promise<T> => {
+import type { Endpoints } from '#common/constants'
+
+type EndpointValue = (typeof Endpoints)[keyof typeof Endpoints]
+
+export const useFetch = async <T>({
+  endpoint,
+  params,
+  context
+}: {
+  endpoint: EndpointValue
+  params: Record<string, string | number>
+  context?: 'android' | 'web6dot0'
+}): Promise<T> => {
   const url = new URL('https://www.jiosaavn.com/api.php')
 
-  url.searchParams.append('__call', type)
+  url.searchParams.append('__call', endpoint.toString())
   url.searchParams.append('_format', 'json')
   url.searchParams.append('_marker', '0')
-  url.searchParams.append('ctx', 'web6dot0')
   url.searchParams.append('api_version', '4')
+  url.searchParams.append('ctx', context || 'web6dot0')
 
   Object.keys(params).forEach((key) => url.searchParams.append(key, String(params[key])))
 
