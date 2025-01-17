9159effd1acd4a8e726ec0f87c65804c64f3adec
Sumit Kolhe <35036894+sumitkolhe@users.noreply.github.com>
2025-01-17 15:53:02 +0530

    chore: add spell checker

---
+++ cspell.config.js
@@ -0,0 +1,19 @@
+export default {
+  version: '0.2',
+  language: 'en',
+  words: [
+    'saavn',
+    'sumit',
+    'kolhe',
+    'jiosaavn',
+    'maxage',
+    'asynciterable',
+    'sumitkolhe',
+    'kbps',
+    'perma',
+    'firstname',
+    'lastname',
+    'stationid'
+  ],
+  ignorePaths: ['**/node_modules/**', 'dist', 'coverage', '.vscode', 'CHANGELOG.md']
+}
